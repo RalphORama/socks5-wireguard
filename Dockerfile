9b51b8728386085e22f2db8cf14a05d4f0df04bd
audrey <6566104+RalphORama@users.noreply.github.com>
2025-04-25 14:14:31 -0400

    fix: 3proxy default config is messed up
    the default 3proxy config on alpine is all sorts of messed up. spent
    some time fixing all that crap so it works now :^)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.20
+FROM alpine:latest
 
 LABEL org.opencontainers.image.title="socks5-wireguard"
 LABEL org.opencontainers.image.description="Create a SOCKS5 server using a WireGuard config"
@@ -14,19 +14,23 @@ RUN apk add --no-cache \
     ca-certificates \
     curl \
     ip6tables \
+    openresolv \
     wireguard-tools
 
 # install 3proxy from testing repo
+# additionally delete the default 3proxy config to avoid issues with it
 RUN apk add \
     --no-cache \
     --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing/ \
-    3proxy
+    3proxy \
+    && \
+    rm -rfv /etc/3proxy/3proxy.cfg /etc/3proxy/conf
 
-RUN mkdir -p /etc/socks5-wireguard /etc/3proxy
+RUN mkdir -p /etc/socks5-wireguard
 
 # Prepare entrypoint and configuration
 COPY entrypoint.sh /usr/local/bin/
-COPY 3proxy.cfg /etc/defaults/3proxy.cfg
+COPY 3proxy.cfg /etc/3proxy/
 
 # Wireguard Options
 ENV WIREGUARD_CONFIG="socks5-wireguard.conf"
@@ -34,8 +38,6 @@ ENV WIREGUARD_CONFIG="socks5-wireguard.conf"
 # 3proxy Ports configuration
 ENV SOCKS5_PROXY_PORT="1080"
 ENV HTTP_PROXY_PORT="3128"
-
-RUN mkdir -p /etc/socks5-wireguard
 
 VOLUME [ "/etc/socks5-wireguard" ]
 VOLUME [ "/etc/3proxy" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
+
+THREEPROXY_CFG='/etc/3proxy/3proxy.cfg'
+WIREGUARD_CONFIG="/etc/socks5-wireguard/${WIREGUARD_CONFIG}"
+
+function log {
+    local LEVEL="$1"
+    local MSG="$(date '+%D %T') [${LEVEL}] $2"
+    case "${LEVEL}" in
+        INFO*)      MSG="\x1B[94m${MSG}";;
+        WARNING*)   MSG="\x1B[93m${MSG}";;
+        ERROR*)     MSG="\x1B[91m${MSG}";;
+        *)
+    esac
+    echo -e "${MSG}"
+}
 
 function spawn {
     if [[ -z ${PIDS+x} ]]; then PIDS=(); fi
@@ -24,39 +39,26 @@ function on_kill {
     kill "${ENTRYPOINT_PID}" 2> /dev/null
 }
 
-function log {
-    local LEVEL="$1"
-    local MSG="$(date '+%D %T') [${LEVEL}] $2"
-    case "${LEVEL}" in
-        INFO*)      MSG="\x1B[94m${MSG}";;
-        WARNING*)   MSG="\x1B[93m${MSG}";;
-        ERROR*)     MSG="\x1B[91m${MSG}";;
-        *)
-    esac
-    echo -e "${MSG}"
-}
-
 export ENTRYPOINT_PID="${BASHPID}"
 
 trap "on_kill" EXIT
 trap "on_kill" SIGINT
-
-WIREGUARD_CONFIG="/etc/socks5-wireguard/${WIREGUARD_CONFIG}"
 
 if ! [ -f "${WIREGUARD_CONFIG}" ]; then
     log "ERROR" "${WIREGUARD_CONFIG} does not exist!"
     exit 1
 fi
 
-if [ ! -f "/etc/3proxy/3proxy.cfg" ]; then
-    log "WARNING" "/etc/3proxy/3proxy.cfg does not exist! Copying it from /etc/defaults/3proxy.cfg"
-    cp "/etc/defaults/3proxy.cfg" "/etc/3proxy/3proxy.cfg"
+if [ ! -f "${THREEPROXY_CFG}" ]; then
+    log "ERROR" "3proxy.cfg does not exist!"
+    exit 1
 fi
 
 log "INFO" "Copying '${WIREGUARD_CONFIG}' to /etc/wireguard/wg0.conf"
 cp -f "${WIREGUARD_CONFIG}" "/etc/wireguard/wg0.conf"
 
-log "INFO" "Starting WireGuard"
+
+log "INFO" "Starting WireGuard..."
 wg-quick up wg0
 
 if ! ip -o -f inet addr show dev wg0; then
@@ -66,13 +68,15 @@ fi
 
 log "INFO" "WireGuard is running"
 
-log "INFO" "Populating values in /etc/3proxy/3proxy.cfg"
-sed -E -i "s/^socks -p[[:digit:]]+$/socks -p${SOCKS5_PROXY_PORT}/" /etc/3proxy/3proxy.cfg
-sed -E -i "s/^proxy -p[[:digit:]]+$/socks -p${HTTP_PROXY_PORT}/" /etc/3proxy/3proxy.cfg
+log "INFO" "Populating values in ${THREEPROXY_CFG}..."
+sed -E -i "s/^socks -p[[:digit:]]+$/socks -p${SOCKS5_PROXY_PORT}/" "${THREEPROXY_CFG}"
+sed -E -i "s/^proxy -p[[:digit:]]+$/socks -p${HTTP_PROXY_PORT}/" "${THREEPROXY_CFG}"
 
 # 3proxy
-log "INFO" "Starting 3proxy"
-spawn 3proxy "/etc/3proxy/3proxy.cfg"
+log "INFO" "Starting 3proxy..."
+spawn 3proxy "${THREEPROXY_CFG}"
 log "INFO" "3proxy is running"
 
+log "INFO" "Executing 'join' function..."
 join
+log "INFO" "Done with 'join' function"
